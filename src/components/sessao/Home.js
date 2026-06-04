0a5dd08730eba8b5cb2ca959f69b2f738dc61d4e
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-06-04 17:51:44 +0200

    Ajuste da palavra entrega

--- src/components/sessao/Home.js
+++ src/components/sessao/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
             <Hero alt={"Imagem da sessão de hero da home"} src={ImgHero} mostrarExplorar={true} imagemFundo={false} mens={"Experimente a essência de Moçambique no conforto da sua casa. Entrega rápida, comida quente e os melhores e os melhores restaurantes da cidade."} titulo={"Sabor à sua"} tituloFoco={" Porta."} />
             <Favoritos />
             <Passos titulo={"O seu prato em 3 passos"} minInfo={""} passos={["Escolha", "Peça", "Receba"]} srcs={[colherGarfo, carrinho, entrega]} alts={["Ícone de colher e garfo cruzados", "Ícone de carrinho de super mercado", "Ícone de carro de entrega"]} descs={["Descricao de cada passo aqui sim aqui apareceram as o passo 1", "Descricao de cada passo aqui sim aqui apareceram o passo 2", "Descricao de cada passo aqui sim aqui apareceram as o passo 3."]} />
-            <Pedido titulo_pedido={"Fome de comida de verdade ?"} alt={"Ícone de Telefone"} src={Phone} desc_pedido={" Peçca agora pelos nossos canais exclusivos e receba descontos especias na sua primeira entrga."} texto={"Pedir via WhatsApp"} tipo={1} estiloBtn={style.btn_fome_pedido} />
+            <Pedido titulo_pedido={"Fome de comida de verdade ?"} alt={"Ícone de Telefone"} src={Phone} desc_pedido={" Peçca agora pelos nossos canais exclusivos e receba descontos especias na sua primeira entrega."} texto={"Pedir via WhatsApp"} tipo={1} estiloBtn={style.btn_fome_pedido} />
         </div>
     );
 }
